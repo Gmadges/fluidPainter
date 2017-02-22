f07e4581d54c00053706cb2f16957119b72ea9b2
George <George.madges@live.co.uk>
2017-02-22 21:38:27 +0000

    got a little dist thing going, nd a vec2

--- src/input.ts
+++ src/input.ts
@@ -1,36 +1,61 @@
 
+class vec2 {
+
+    public x : number;
+    public y : number;
+
+    constructor( _x : number, _y : number) {
+        this.x = _x;
+        this.y = _y;
+    }
+
+    public distance( pos : vec2) : any {
+        return new vec2(this.x - pos.x, this.y - pos.y);
+    }
+}
+
 class InputController {
 
     private forceApplied : boolean = false;
     private bMouseDown : boolean = false;
 
+    private lastPos : vec2 = new vec2(0,0);
+    private currentPos : vec2 = new vec2(0,0);
+
     constructor(private canvas: HTMLCanvasElement) {
 
-        canvas.onmousedown = function(e){
-            this.bMouseDown = true;
-        }.bind(this);
-
-        canvas.onmouseup = function(e){
-            this.bMouseDown = false;
-        }.bind(this);
-
-        canvas.onmousemove = function(e){
-            if(!this.bMouseDown) return;
-            
-            this.getCursorPosition(canvas, e);
-            return false;
-        }.bind(this);
-
+        canvas.onmousedown = this.mouseDown.bind(this);
+        canvas.onmouseup = this.mouseUp.bind(this);
+        canvas.onmousemove = this.mouseMove.bind(this);
     }
 
     public isForceApplied() : boolean {
         return false;
     }
 
-    private getCursorPosition(canvas, event) {
+    private mouseUp(e : Event) {
+        this.bMouseDown = false;
+    }
+
+    private mouseDown(e : Event) {
+        this.bMouseDown = true;
+    }
+
+    private mouseMove(e : Event) {
+        if(!this.bMouseDown) return;
+        
+        this.lastPos = this.currentPos;
+
+        this.currentPos = this.getCursorPosition(this.canvas, e);
+
+        // calc the distance
+        var dist : vec2 = this.currentPos.distance(this.lastPos);
+
+        console.log("dist x: " + dist.x + " y: " + dist.y);
+    }
+
+    private getCursorPosition(canvas, event) : any {
         var rect = canvas.getBoundingClientRect();
-        var x = event.clientX - rect.left;
-        var y = event.clientY - rect.top;
-        console.log("x: " + x + " y: " + y);
+        return new vec2(event.clientX - rect.left, event.clientY - rect.top);
     }
 }
