66b3d34d11ca705148afd0e0a0959a7e324bfdc6
George <George.madges@live.co.uk>
2017-02-24 20:06:50 +0000

    sweet gulp build setup working

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var tsc = require('gulp-typescript');
+var browserSync = require('browser-sync');
+//var exec = require('gulp-exec');
+var execSync = require('child_process').execSync;
+
+var tsProject = tsc.createProject('tsconfig.json');
+
+gulp.task('typescript', function () {
+    return gulp.src('./src/**/*.ts')
+        .pipe(tsProject())
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('emscripten', function () {
+    execSync('make clean && make');
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.ts', ['typescript']);
+  gulp.watch(['./src/**/*.{cpp,h,hpp}', './shaders/**/*.frag'], ['emscripten']);
+});
+
+gulp.task('default', ['emscripten', 'typescript', 'browser-sync', 'watch']);
