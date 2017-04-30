3013bf8c8c16b1ccaecaffbe7207557beebdb907
gmadges <gmadgesdeveloper@gmail.com>
2017-04-30 17:01:38 +0100

    almost there cannot get the last few tests passing

--- src/undo.ts
+++ src/undo.ts
@@ -3,16 +3,18 @@
 // this class is designed to act like a queue / stack but without the creation and deletion of objects. 
 // this is because i want to generate a set amount of buffers at the beginning and keep reususing them.
 // gonna do this circular buffer style
+// this will only work nicely on object arrays due to javascript passing anything that isnt and object by value not ref.
 class Undo {
     
     private undoQueue : any[] = [];
     private currentIndex : number = 0;
     private maxSize : number = 0;
+    private undoDepth : number = 0;
 
     private undoEnabled : boolean = false;
     private redoEnabled : boolean = false;
 
-    constructor(data : [any]) {
+    constructor(data : [object]) {
         this.undoQueue = data;
         this.maxSize = data.length;
     };
@@ -25,18 +27,59 @@ class Undo {
         return this.maxSize;
     }
 
+    // this can be used for finding out if we're on our first undo or not.
+    public isRedoEnabled() : boolean {
+        return this.redoEnabled;
+    }
+
+    // this gives the current item. to be used before first undo.
+    // must set this if you want to use redo!!!!!!
+    public getCurrentItem() : any {
+        let index : number = this.currentIndex + 1;
+        if(index > this.maxSize) {
+            this.undoDepth = index;
+            return this.doShift();
+        };
+        this.undoDepth = this.currentIndex;
+        return this.undoQueue[this.currentIndex];
+    };
+
     // returns item that can be overwritten
     public getItemToStoreTo() : any {
-        // shift everything down
-        let returnItem = this.undoQueue[this.currentIndex];
-        this.currentIndex++;
+
+        // unod can now happen because we have values to revert to
         this.undoEnabled = true;
-        return returnItem;
+        // we now cannot redo because we are at the head.
+        this.redoEnabled = false;
+
+        // increase the index
+        this.currentIndex++;
+
+        if(this.currentIndex > this.maxSize) {
+            return this.doShift();
+        }
+        
+        return this.undoQueue[this.currentIndex - 1];
+    };
+
+    private doShift() : any {
+        // perform a shift
+        let tmp = this.deepCopy(this.undoQueue[0]);
+
+        for(let i = 0; i < (this.maxSize - 1); i++) {
+            this.undoQueue[i] = this.deepCopy(this.undoQueue[i+1]);
+        };
+
+        this.currentIndex = this.maxSize;
+        this.undoQueue[this.currentIndex] = tmp;
+        return tmp;
     };
 
     // returns undo item
     public undo() : any {
         if(this.undoEnabled === false) return null;
+        // we have undone so we can redo now.
+        this.redoEnabled = true;
 
         if(this.currentIndex > 0) {
             this.currentIndex--;
@@ -48,9 +91,13 @@ class Undo {
     public redo() : any {
         if(this.redoEnabled === false) return null;
 
-        if(this.currentIndex < this.maxSize) {
+        if(this.currentIndex < this.undoDepth) {
             this.currentIndex++;
         };
         return this.undoQueue[this.currentIndex];
     };
+
+    private deepCopy(obj : Object) : any {
+        return JSON.parse(JSON.stringify(obj));
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 var fs = require('fs');
 const resolve = require('path').resolve;
 
@@ -7,7 +7,7 @@ eval(fs.readFileSync(resolve('./app/undo.js')).toString());
 
 describe('Vec2', function() {
   
-  describe('#initialise', function() {
+  describe('initialise', function() {
     it('should return correct values', function() {
       var testVec = new vec2(10, 5);
       assert.equal(10, testVec.x);
@@ -15,14 +15,14 @@ describe('Vec2', function() {
     });
   });
 
-  describe('#length', function() {
+  describe('length', function() {
     it('should return correct values', function() {
       var testVec = new vec2(3, 4);
       assert.equal(5, testVec.length());
     });
   });
 
-  describe('#distance', function() {
+  describe('distance', function() {
     it('should return correct values', function() {
       var posA = new vec2(56, 74);
       var posB = new vec2(32, 128);
@@ -40,134 +40,272 @@ describe('UndoBuffers', function() {
   var undo;
 
   beforeEach(function() {
-    undo = new Undo([0,0,0,0,0]);
+    undo = new Undo([{val:0},{val:0},{val:0},{val:0},{val:0}]);
   });
   
-  describe('#initialise', function() {
+  describe('initialise', function() {
     it('initialise values to be correct', function() {
-      assert.equal(0, undo.getCurrentIndex());
-      assert.equal(5, undo.getMaxQueueSize());
+      assert.strictEqual(0, undo.getCurrentIndex());
+      assert.strictEqual(5, undo.getMaxQueueSize());
 
       // nothing has been stored. so we shouldnt be able to perform these actions
-      assert.equal(null , undo.undo());
-      assert.equal(null , undo.redo());
+      assert.typeOf(undo.undo(), 'null', 'undo returns null');
+      assert.typeOf(undo.redo(), 'null', 'redo returns null');
     });
   });
 
-  describe('#storing index', function() {
-    it('correct index after storage methods', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
-      undo.getItemToStoreTo() = 3;
-      undo.getItemToStoreTo() = 2;
+  describe('storing', function() {
+    describe('index', function() {
+      it('check index after storage', function() {
+        
+        var item = undo.getItemToStoreTo();
+        // we stored numbers so we'd expect them here
+        assert.typeOf(item.val, 'number');
+        assert.strictEqual(0, item.val);
+        assert.strictEqual(1, undo.getCurrentIndex());
 
-      assert.equal(3, undo.getCurrentIndex());
-      
-      undo.getItemToStoreTo() = 1;
-      undo.getItemToStoreTo() = 0;
+        undo.getItemToStoreTo();
+        undo.getItemToStoreTo();
 
-      assert.equal(4, undo.getCurrentIndex());
+        assert.strictEqual(3, undo.getCurrentIndex());
+      });
+    });
+
+    describe('item', function() {
+      it('correct index after storage methods', function() {
+
+        // should be full of zeros
+        assert.strictEqual(0, undo.getItemToStoreTo().val);
+        assert.strictEqual(0, undo.getItemToStoreTo().val);
+        assert.strictEqual(0, undo.getItemToStoreTo().val);
+        assert.strictEqual(0, undo.getItemToStoreTo().val);
+        assert.strictEqual(0, undo.getItemToStoreTo().val);
+
+        // now should be full of numbers
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 1;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 2;
+        var item3 = undo.getItemToStoreTo();
+        item3.val = 3;
+        var item4 = undo.getItemToStoreTo();
+        item4.val = 4;
+        var item5 = undo.getItemToStoreTo();
+        item5.val = 5;
+
+        // next item to overwrite will be the last in the pack which should be 1 etc
+        assert.strictEqual(1, undo.getItemToStoreTo().val);
+        assert.strictEqual(2, undo.getItemToStoreTo().val);
+        assert.strictEqual(3, undo.getItemToStoreTo().val);
+        assert.strictEqual(4, undo.getItemToStoreTo().val);
+        assert.strictEqual(5, undo.getItemToStoreTo().val);
+      });
     });
   });
 
-  describe('#storing item', function() {
-    it('correct index after storage methods', function() {
+  describe('undo', function() { 
+    describe('basic', function() {
+      it('should return correct values after some basic undo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
 
-      // should be full of zeros
-      assert.equal(0, undo.getItemToStoreTo());
-      assert.equal(0, undo.getItemToStoreTo());
-      assert.equal(0, undo.getItemToStoreTo());
-      assert.equal(0, undo.getItemToStoreTo());
-      assert.equal(0, undo.getItemToStoreTo());
+        var item = undo.undo();
+        assert.equal(4, item.val);
+        assert.equal(1, undo.getCurrentIndex());
+      });
+    });
 
-      // now should be full of numbers
-      undo.getItemToStoreTo() = 1;
-      undo.getItemToStoreTo() = 2;
-      undo.getItemToStoreTo() = 3;
-      undo.getItemToStoreTo() = 4;
-      undo.getItemToStoreTo() = 5;
+    describe('advanced 1', function() {
+      it('should return correct values after some undo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
 
-      // next item to overwrite will be the last in the pack which should be 1
-      assert.equal(1, undo.getItemToStoreTo());
+        undo.undo();
+        undo.undo();
+        var item = undo.undo();
+        // 3 undos should still return 5, its the oldest value we have
+        assert.equal(5, item.val);
+        assert.equal(0, undo.getCurrentIndex());
+      });
+    });
+
+    describe('advanced 2', function() {
+      it('should return correct values after some undo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
+
+        var item = undo.undo();
+        assert.equal(4, item.val);
+        assert.equal(1, undo.getCurrentIndex());
+      });
+    });
+
+    describe('advanced 3', function() {
+      it('should return correct values after some undo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
+
+        undo.undo();
+        var item3 = undo.getItemToStoreTo();
+        item3.val = 3;
+
+
+        assert.equal(3, undo.undo().val);
+        assert.equal(5, undo.undo().val);
+      });
     });
   });
 
-  describe('#undo basic', function() {
-    it('should return correct values after some basic undo ops', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
+  describe('redo basic ', function() { 
+    describe('redo basic ', function() {
+      it('should return correct values after some redo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
 
-      var item = undo.undo();
-      assert.equal(4, item);
-      assert.equal(1, undo.getCurrentIndex());
+        // shouldnt be able to redo unless we have undone
+        assert.equal(null, undo.redo());
+        assert.equal(false, undo.isRedoEnabled());
+
+        var item = undo.getCurrentItem();
+        item.val = 7;
+
+        assert.equal(4, undo.undo().val);
+
+        assert.equal(7, undo.redo().val);
+        assert.equal(2, undo.getCurrentIndex());
+      });
+    });
+
+    describe('redo basic ', function() {
+      it('should return correct values after some redo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
+
+        var item = undo.getCurrentItem();
+        item.val = 7;
+
+        assert.equal(4, undo.undo().val);
+
+        assert.equal(7, undo.redo().val);
+        assert.equal(7, undo.redo().val);
+        assert.equal(7, undo.redo().val);
+      });
+    });
+
+    describe('redo advanced 1 ', function() {
+      it('should return correct values after some redo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
+
+        undo.undo();
+        var item = undo.getItemToStoreTo();
+        item.val = 3;
+
+        assert.equal(null, undo.redo());
+      });
+    });
+
+    describe('redo advanced 2 ', function() {
+      it('should return correct values after some redo ops', function() {
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 5;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 4;
+
+        var item = undo.getCurrentItem();
+        item.val = 7;
+        
+        undo.undo();
+        undo.undo();
+
+        assert.equal(4, undo.redo().val);
+      });
     });
   });
 
-  describe('#undo advanced', function() {
-    it('should return correct values after some undo ops', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
+  describe('common usage', function() {
 
-      undo.undo();
-      undo.undo();
-      var item = undo.undo();
-      // 3 undos should still return 5, its the oldest value we have
-      assert.equal(5, item);
-      assert.equal(0, undo.getCurrentIndex());
+    describe('pattern 1', function() {
+        it('should pass these tests of standard usage', function() {
+          
+        // store two items
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 37;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 24;
+
+        // snapshot current item
+        var item3 = undo.getCurrentItem();
+        item3.val = 7;
+
+        // undo
+        undo.undo();
+
+        // second undo
+        assert.equal(37, undo.undo().val);
+        // spam the redo
+        assert.equal(24, undo.redo().val);
+        assert.equal(7, undo.redo().val);
+        assert.equal(7, undo.redo().val);
+      });
     });
-  });
 
-  describe('#undo advanced 1', function() {
-    it('should return correct values after some undo ops', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
+    describe('pattern 2', function() {
+        it('should pass these tests of standard usage', function() {
+          
+        // store two items
+        var item1 = undo.getItemToStoreTo();
+        item1.val = 37;
+        var item2 = undo.getItemToStoreTo();
+        item2.val = 24;
+        var item3 = undo.getItemToStoreTo();
+        item3.val = 50;
+        var item4 = undo.getItemToStoreTo();
+        item4.val = 96;
+        var item5 = undo.getItemToStoreTo();
+        item5.val = 34;
+        console.log(undo.getCurrentIndex());
+        console.log(undo.undoQueue);
+        var item6 = undo.getItemToStoreTo();
+        item6.val = 93;
+        console.log(undo.getCurrentIndex());
+        console.log(undo.undoQueue);
+        var item7 = undo.getItemToStoreTo();
+        item7.val = 1;
+        console.log(undo.getCurrentIndex());
+        console.log(undo.undoQueue);
+        // redo shouldnt work
+        assert.equal(null, undo.redo());
+        
+        // snapshot current item
+        var item8 = undo.getCurrentItem();
+        item8.val = 12;
 
-      var item = undo.undo();
-      assert.equal(4, item);
-      assert.equal(1, undo.getCurrentIndex());
+
+        // spam undo
+        assert.equal(1, undo.undo().val);
+        assert.equal(93, undo.undo().val);
+        assert.equal(34, undo.undo().val);
+        assert.equal(93, undo.redo().val);
+        assert.equal(34, undo.undo().val);
+        assert.equal(96, undo.undo().val);
+        assert.equal(96, undo.undo().val);
+      });
     });
+    // todo could add more tests
   });
-
-  describe('#undo advanced 2', function() {
-    it('should return correct values after some undo ops', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
-
-      undo.undo();
-      undo.getItemToStoreTo() = 3;
-
-      assert.equal(3, undo.undo());
-      assert.equal(5, undo.undo());
-    });
-  });
-
-  describe('#redo basic ', function() {
-    it('should return correct values after some redo ops', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
-
-      // shouldnt be able to redo unless we have undone
-      assert.equal(null, undo.redo());
-
-      undo.undo();
-      var item = undo.redo();
-      assert.equal(4, item);
-      assert.equal(1, undo.getCurrentIndex());
-    });
-  });
-
-  describe('#redo advanced 1 ', function() {
-    it('should return correct values after some redo ops', function() {
-      undo.getItemToStoreTo() = 5;
-      undo.getItemToStoreTo() = 4;
-
-      undo.undo();
-      undo.getItemToStoreTo() = 3;
-
-      assert.equal(null, undo.redo());
-    });
-  });
-
-// todo MORE test
-
 });
